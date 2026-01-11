100a966ca289d55314c6c5e53b03504eba2a3d38
boytrend0108 <boytrend0108@users.noreply.github.com>
2026-01-11 05:40:27 +0200

    feat: create prisma models

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
-
-// Load .env file from the project root
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
